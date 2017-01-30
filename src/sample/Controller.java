1b1b2451c764a45d4f90b3d226883d8e575c2016
henriklarsen <s315317@stud.hioa.no>
2017-01-30 11:59:34 +0100

    Color

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,33 @@
 package sample;
 
+
+import javafx.scene.control.*;
+import javafx.fxml.FXML;
+import javafx.scene.paint.Color;
+import javafx.event.ActionEvent;
+
 public class Controller {
+    @FXML private Button startButton;
+    @FXML private Button pauseButton;
+    @FXML private Button resetButton;
+    @FXML private Slider speedSlider;
+    @FXML private ColorPicker cellColorPicker;
+    @FXML private ColorPicker backgroundColorPicker;
+    @FXML private Button gridToggleButton;
+    @FXML private TextField sizeInputField;
+    @FXML private MenuBar menuBar;
+
+    private Color currentCellColor = Color.BLACK;
+    private Color currentBackgroundColor = Color.WHITE;
+
+    public void trueCellColor(){
+        currentCellColor = cellColorPicker.getValue();
+        System.out.println(currentCellColor);
+    }
+
+    public void trueBackgroundColor(){
+        currentBackgroundColor = backgroundColorPicker.getValue();
+        System.out.println(currentBackgroundColor);
+    }
+
 }
