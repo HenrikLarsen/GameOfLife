0397a8cf5ef66d8057860857dab8770e62c4bd3e
V1adau <Oscarnvh@gmail.com>
2017-05-04 10:28:17 +0200

    ikon

--- src/model/Main.java
+++ src/model/Main.java
@@ -1,13 +1,12 @@
 package model;
 
 import controller.MainWindowController;
+import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
-
-import javax.swing.*;
 
 /**
  * This is the driver class for this implementation of Game of Life. It contains the start method from
@@ -17,7 +16,7 @@ import javax.swing.*;
  * @author Henrik Finnerud Larsen
  * @version 1.0
  */
-public class Main extends javafx.application.Application {
+public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -28,13 +27,6 @@ public class Main extends javafx.application.Application {
         primaryStage.sizeToScene();
         primaryStage.getIcons().add(new Image(MainWindowController.class.getResourceAsStream("/resources/" +
                 "images/GameofLife2.png")));
-
-        com.apple.eawt.Application macApp = com.apple.eawt.Application.getApplication();
-        macApp.setDockIconImage (new ImageIcon (getClass ().
-                getResource ("/resources/" +
-                        "images/GameofLife2.png")).
-                getImage ());
-
         primaryStage.show();
 
         //Makes sure that ThreadWorker's shutDownExecutor() method is called when JVM shuts down
